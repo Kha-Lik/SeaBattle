a39c2e6bc05e2764b963fc144ae6b2b009fefce6
MykolaV <kolya.vasevich@gmail.com>
2020-11-23 02:56:53 +0200

    added battlefield builder

---
+++ SeaBattle/BattlefieldBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace SeaBattle
+{
+    public abstract class BattlefieldBuilder
+    {
+        protected readonly GameSettings Settings;
+        public BattlefieldBuilder(GameSettings settings)
+        {
+            Settings = settings;
+        }
+
+        public abstract void BuildBattlefield();
+
+        public abstract void PlaceShips();
+
+        protected Dictionary<int, int> GetShipsFromSettings(GameSettings settings)
+        {
+            Dictionary<int, int> shipsSettings = new();
+            foreach (var (size, count) in settings.Ships)
+            {
+                if (int.TryParse(size, out var result))
+                    shipsSettings.Add(result, count);
+            }
+
+            return shipsSettings;
+        }
+    }
+}
---
+++ SeaBattle/GameSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SeaBattle
+{
+    public class GameSettings
+    {
+        public int FieldSize { get; set; }
+        
+        //size, count
+        public Dictionary<string, int> Ships { get; set; }
+    }
+}
