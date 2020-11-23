5bbf898bd817c3693cef744aae162e6e9dd546c0
MykolaV <kolya.vasevich@gmail.com>
2020-11-23 02:54:24 +0200

    extracted interface IBattlefield, added method for getting neighbours of cell

--- SeaBattle/Battlefield.cs
+++ SeaBattle/Battlefield.cs
@@ -1,8 +1,11 @@
-﻿using System.Drawing;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 
 namespace SeaBattle
 {
-    public class Battlefield
+    public class Battlefield : IBattlefield
     {
         private Cell[,] _field;
 
@@ -19,6 +22,36 @@ namespace SeaBattle
         {
             get => _field[point.X, point.Y];
             set => _field[point.X, point.Y] = value;
+        }
+
+        public bool IsPointInField(Point point)
+        {
+            return (point.X >= 0 && point.Y >= 0) && (point.X < Size && point.Y < Size);
+        }
+
+        public ICollection<Cell> GetNeighbours(Cell cell)
+        {
+            var neighs = new List<Cell>();
+
+            var startPoint = new Point {X = cell.Coordinates.X - 1, Y = cell.Coordinates.Y - 1};
+            var neighPoints = new List<Point>();
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    var point = new Point {X = startPoint.X + i, Y = startPoint.Y + j};
+                    if (IsPointInField(point) && point != cell.Coordinates)
+                        neighPoints.Add(point);
+                }
+            }
+            
+            neighs.AddRange(neighPoints.Select(p => this[p]));
+            return neighs;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _field.GetEnumerator();
         }
     }
 }
---
+++ SeaBattle/IBattlefield.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace SeaBattle
+{
+    public interface IBattlefield : IEnumerable
+    {
+        int Size { get; set; }
+        Cell this[Point point] { get; set; }
+        bool IsPointInField(Point point);
+        ICollection<Cell> GetNeighbours(Cell cell);
+    }
+}
