4b08f6d827d9fe4c2448450f111072737906785a
MykolaV <kolya.vasevich@gmail.com>
2020-11-25 00:18:22 +0200

    added player constructor

---
+++ SeaBattle/IPlayerBuilder.cs
@@ -0,0 +1,7 @@
+﻿namespace SeaBattle
+{
+    public interface IPlayerBuilder
+    {
+        IPlayer ConstructPlayer();
+    }
+}
---
+++ SeaBattle/PlayerBuilder.cs
@@ -0,0 +1,28 @@
+﻿namespace SeaBattle
+{
+    public class PlayerBuilder : IPlayerBuilder
+    {
+        private readonly BattlefieldBuilder _battlefieldBuilder;
+
+        public PlayerBuilder(BattlefieldBuilder battlefieldBuilder)
+        {
+            _battlefieldBuilder = battlefieldBuilder;
+        }
+
+        public IPlayer ConstructPlayer()
+        {
+            Player player = new();
+            player.Battlefield = ConstructBattlefield();
+            player.AutoShotMethod = new RandomShot(player);
+
+            return player;
+        }
+
+        private IBattlefield ConstructBattlefield()
+        {
+            _battlefieldBuilder.BuildBattlefield();
+            _battlefieldBuilder.PlaceShips();
+            return _battlefieldBuilder.GetResult();
+        }
+    }
+}
