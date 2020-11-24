3f17a1e5de50c7dd94f13269396accd1c575473a
MykolaV <kolya.vasevich@gmail.com>
2020-11-25 00:18:43 +0200

    added Game class

---
+++ SeaBattle/Game.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+
+namespace SeaBattle
+{
+    public class Game : IGame
+    {
+        private IPlayer _playerOne;
+        private IPlayer _playerTwo;
+
+        public Game(IPlayerBuilder playerBuilder)
+        {
+            _playerOne = playerBuilder.ConstructPlayer();
+            _playerTwo = playerBuilder.ConstructPlayer();
+        }
+
+        public IBattlefield GetPlayerOneField() => _playerOne.Battlefield;
+
+        public IBattlefield GetPlayerTwoField() => _playerTwo.Battlefield;
+
+        public bool AutoShoot()
+        {
+            if (_playerOne.IsMyTurn)
+            {
+                _playerOne.IsMyTurn = false;
+                _playerTwo.IsMyTurn = true;
+
+                return _playerOne.AutoShot(_playerTwo.Battlefield);
+            }
+            
+            _playerOne.IsMyTurn = true;
+            _playerTwo.IsMyTurn = false;
+
+            return _playerTwo.AutoShot(_playerOne.Battlefield);
+        }
+
+        public bool ManualShoot(Point target)
+        {
+            if (_playerOne.IsMyTurn)
+            {
+                _playerOne.IsMyTurn = false;
+                _playerTwo.IsMyTurn = true;
+
+                return _playerOne.ManualShot(target, _playerTwo.Battlefield);
+            }
+            
+            _playerOne.IsMyTurn = true;
+            _playerTwo.IsMyTurn = false;
+
+            return _playerTwo.ManualShot(target, _playerOne.Battlefield);
+        }
+    }
+}
---
+++ SeaBattle/IGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace SeaBattle
+{
+    public interface IGame
+    {
+        IBattlefield GetPlayerOneField();
+        
+        IBattlefield GetPlayerTwoField();
+        
+        bool AutoShoot();
+        
+        bool ManualShoot(Point target);
+        
+    }
+}
